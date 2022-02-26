aac7e6752137735ebe24d18d3ea7495eec6f8359
lillyleiran <lilly.leiran@gmail.com>
2022-02-26 11:37:30 -0700

    add functions

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -19,3 +19,19 @@ test('creates a player object', () => {
     );
 });
 
+test('get players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('Dave');
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+});
